5425625819087e389a8dd8eb727e3632b6b41f90
devWillS <>
2022-03-20 19:06:34 +0900

    4-2-2 バリデーションルール

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,6 @@ class UserController extends Controller
         return csrf_token(); 
     }
 
-    // 引数でUserRegistPostクラスのインスタンスを渡す
     public function register(Request $request)
     {
         // すべての入力値を取得し$inputsに保持する
@@ -24,9 +23,33 @@ class UserController extends Controller
         // バリデーションルールを定義する
         // nameキーの値は必須とし、ageは整数型とする
         $rules = [
-            'name' => 'required',
+            'name' => 'required', // 必須項目である
             'email' => ['required','email'],  // 藍列で指定（必須かつメールアドレスであること）
             'age' => 'required|integer', // パイプラインで区切る（必須かつ整数値）
+
+            'user_no' => 'numeric', // 数値であることを確認
+            'alpha' => 'alpha', // 全て英字であることを確認
+            'email' => "email", // メールアドレス形式であることを確認
+            'ip_address' => 'ip', // IPアドレス形式であることを確認
+
+            'bank_pass' => 'digits:4', // 4桁の数値
+            'signal_color' => 'in:green,red,yellow', // 3種のうちいずれかであることを確認
+
+            'number' => ['integer', 'size:10'], // 10であることを確認
+            'name' => 'size:10', // 10文字であることを確認
+            'button' => ['array', 'size:10'], // 要素数10であることを確認
+            'upload' => ['file', 'size:10'], // ファイルサイズ10KBであることを確認
+
+            'user_id' => 'regex:/^[0-9a-zA-Z]+$/', // 半角英数字
+
+            'email' => 'confirmed', // emailとemail_confirmationが同値であることを確認
+
+            'name' => 'unique:users', // usersテーブルの同名カラム(name)と比較して重複しないことを確認
+            'mail' => 'unique:users,email', // usersテーブルのemailカラムとmailフィールドの値を比較して重複しないことを確認
+            'mail' => 'unique:users,email,100', // 上記と同じルールだが,idが100のレコードは重複を許可する
+
+            'email' => ['bail', 'required'], // bailルールを使うと、バリデーションエラーの場合に以降のバリデーションを行わない
+            'name' => ['required', 'max:255'],
         ];
 
         // バリデータクラスのインスタンスを取得
