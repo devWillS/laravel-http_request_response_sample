ea5dc5cc230b4edc11c913b79a22a20989a3d3fa
devWillS <>
2022-03-20 16:24:19 +0900

    4-1-1 リクエストの取得

--- public/index.php
+++ public/index.php
@@ -48,6 +48,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
+/// リクエスト取り扱い部分
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
